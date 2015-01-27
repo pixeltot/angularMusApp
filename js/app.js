e4c0b5f0f591cc91b3f0e075a2ca4968d7613a27
AJ <pixeltot@gmail.com>
2015-01-26 16:23:51 -0800

    angular music app
    prototype

---
+++ js/app.js
@@ -0,0 +1,61 @@
+(function(){
+ 
+  var app = angular.module('musicApp', []);
+  var gems = [
+   {name: 'Gunman', 
+   price: 2.95, 
+   description:"This is one cool gunman poster",
+   shine: 8,
+   review:"wow what a cool poster, totally radical!",
+   images:[
+   "images/gunman-1.jpg", 
+   "images/gunman-2.jpg",
+   "images/gunman-3.jpg"]
+   },
+   {name:'Workout', 
+   price: 2.95, 
+   description:"Work it baby",
+   shine: 4,
+   review:"delicous pictures of edible workouts",
+   images:[
+   "images/workout-1.jpg", 
+   "images/workout-2.jpg"]
+   },
+   {name:'Buddah', 
+   price:5.95, 
+   description:"buddah says, I am I am, a real religous man",
+   shine:7,
+   review:"I love this buddah poster, get them all in different colors too!",
+   images:[
+   "images/buddah-1.jpg", 
+   "images/buddah-2.jpg",
+   "images/buddah-3.jpg"]}
+   ];
+   
+   
+  app.controller('StoreController',function(){
+  	this.products = gems;  	
+  });
+  
+  app.controller('TabController', function(){
+    this.tab = 1;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
+  
+  app.directive('pixeltotBanner', function(){
+  	return{
+  		restrict:'E',
+  		templateUrl: "pixeltot-banner.html"
+  	}	
+  });
+  
+  
+})();
+
